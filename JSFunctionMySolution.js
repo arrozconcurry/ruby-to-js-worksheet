5ecc054c0de480dbd828b4fc5e38c6ec9f880870
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-15 14:11:21 -0700

    exercise 2

--- JSFunctionMySolution.js
+++ JSFunctionMySolution.js
@@ -1,3 +1,4 @@
+// Ex. 1
 var toDo = {
   description: 'homework',
   assignee: 'kaitlin',
@@ -7,3 +8,18 @@ var toDo = {
   }
 };
 toDo.printStatus();
+
+// Ex. 2
+var arrayOfNums = [2, 7, 7, 3, 9, 0, 1, 6, 8, 3, 8, 4, 9];
+var getBiggest = function(array) {
+  var max = array[0];
+  array.forEach(function(num) {
+    if (num >= max) {
+      max = num;
+    }
+  });
+  return max;
+};
+
+var biggest = getBiggest(arrayOfNums);
+console.log('The biggest is: ', biggest);
