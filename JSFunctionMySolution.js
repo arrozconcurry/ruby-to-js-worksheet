16f4d239502512e5490984acc430ad81b3dd6d72
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-15 14:18:13 -0700

    shortened version

--- JSFunctionMySolution.js
+++ JSFunctionMySolution.js
@@ -11,15 +11,21 @@ toDo.printStatus();
 
 // Ex. 2
 var arrayOfNums = [2, 7, 7, 3, 9, 0, 1, 6, 8, 3, 8, 4, 9];
-var getBiggest = function(array) {
-  var max = array[0];
-  array.forEach(function(num) {
-    if (num >= max) {
-      max = num;
-    }
-  });
-  return max;
-};
+// var getBiggest = function(array) {
+//   var max = array[0];
+//   array.forEach(function(num) {
+//     if (num >= max) {
+//       max = num;
+//     }
+//   });
+//   return max;
+// };
+//
+// var biggest = getBiggest(arrayOfNums);
+// console.log('The biggest is: ', biggest);
 
-var biggest = getBiggest(arrayOfNums);
-console.log('The biggest is: ', biggest);
+// OR
+arrayOfNums.sort(function(a, b) {
+  return a - b;
+});
+console.log('The biggest is: ', arrayOfNums.pop());
