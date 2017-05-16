c601d786324c2738069f1f79fd382e4666b442c8
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-16 10:19:52 -0700

    explanation for part 2. 3.

--- mySolutionsDay2.js
+++ mySolutionsDay2.js
@@ -48,6 +48,35 @@ output(['potato', 'gravy', 'turkey']);
 
 // PART 2
 // 1.
-
+var zombies = function() {
+  return "We like to eat people";
+};
 
 // 2.
+var Calculator = function() {
+  this.square = function (x) {
+    return x * x;
+  };
+};
+var my_calculator = new Calculator();
+
+// 3.
+console.log(function () {
+  return "Hey hey hey";
+}());
+/* `}()` anonymous function call
+ anything that is a function,
+ can be invoked with paranthesis.
+ If `}` w/o the `()` output is [Function]
+ If a named function, JS would have given us
+ [name] instead of [Function].
+ Difference between a function object vs. calling a function 
+ and working with return value.
+ Quickly identify which one you're working with
+*/
+
+// 4.
+var blabbermouth = function() { };
+console.log(blabbermouth.name);
+
+// 5.
